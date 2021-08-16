33d0e1316a41b23c36d686861612305bcdf274f0
micko45 <michaelmcgearty@gmail.com>
2021-08-16 13:25:41 +0100

    update pandas to remove colwidht error

--- get_ads.py
+++ get_ads.py
@@ -5,7 +5,7 @@ import pandas as pd
 import pickle
 import mail_df
 
-pd.set_option('display.max_colwidth', -1)
+pd.set_option('display.max_colwidth', None)
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', None)
